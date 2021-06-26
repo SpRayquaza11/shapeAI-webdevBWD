ba6fd0e26bcd68e742152f741d7e0d7be9e9cacb
SpRayquaza11 <74767746+SpRayquaza11@users.noreply.github.com>
2021-06-26 21:29:56 +0530

    Add files via upload
    commit 1

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function info() {
+  return (
+    <div className="note">
+      <h1> javascript and react.js</h1>
+      <p> basic web dev and react js bootcamp </p>
+    </div>
+  );
+}
+
+export default info;
